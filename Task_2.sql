808dcb4367489cf6e808c28f1560e08d2f2e99f6
CalvinMathews <calvinriyadh@gmail.com>
2025-03-23 23:07:48 +0530

    Task 2 is being worked on and some other changes

--- Task_2.sql
+++ Task_2.sql
@@ -1,34 +1,14 @@
--- retrieve all customers
-SELECT * FROM [User];
+--Task 2: Select,Where
 
--- all couriers sent by a specific customer
-SELECT * FROM Courier WHERE SenderName = 'Alice Kumar';
+--1. List all customers:
+	SELECT * FROM [USER];
 
+--2. List all orders for a specific customer:
+	sp_help	courier
+	SELECT * FROM Courier WHERE SenderName = 'Amit Sharma';
 
--- all courier records
-SELECT * FROM Courier;
+--3. List all couriers:
+	SELECT * FROM Courier;
 
-
---  payment and location details for a specific courier
-SELECT * FROM Payment WHERE CourierID = 3;
-
--- undelivered packages list
-SELECT * FROM Courier WHERE Status != 'Delivered';
-
--- couriers scheduled for delivery today
-SELECT * FROM Courier WHERE DeliveryDate = convert(DATE, getdate());
-
--- package specific status
-SELECT * FROM Courier WHERE Status like '%Transit%';
-
--- total number of packages
-SELECT SenderName, COUNT(CourierID) FROM Courier GROUP BY SenderName;
-
--- specific weight range
-select courierID, weight from Courier where weight between 0 and 5
-
--- contains JOhn
-SELECT * FROM Employee WHERE Name LIKE '%Mathews%';
-
--- courier > 50
-SELECT * FROM Courier WHERE courierID in (select CourierID from payment where amount > 330);
+--4. List all packages for a specific order:
+	
